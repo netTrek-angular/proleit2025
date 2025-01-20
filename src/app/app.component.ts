614095d2095d07a87cc11a30830a28ea8262e4bc
Saban Ünlü [netTrek] <us@netTrek.de>
2025-01-20 16:49:25 +0100

    feat: header-components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {HelloWorldComponent} from './hello-world.component';
+import {HeaderComponent} from './header/header.component';
 
 @Component({
   selector: 'pl-root',
   imports: [
     RouterOutlet,
-    HelloWorldComponent
+    HelloWorldComponent,
+    HeaderComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/hello-world.component.ts
+++ src/app/hello-world.component.ts
@@ -1,8 +1,14 @@
-import {Component} from '@angular/core';
+import {Component, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'pl-hello-world',
-  template: '<h1>Hello World</h1>'
+  template: '<h1>Hello World</h1>',
+  styles: `h1 {
+    color: red;
+  }`,
+  // encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated, /*default*/
+  // encapsulation: ViewEncapsulation.ShadowDom
 })
 export class HelloWorldComponent {
 }
