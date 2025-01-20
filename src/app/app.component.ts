823577d741cda6a053169377e969a842426bdcc3
Saban Ünlü [netTrek] <us@netTrek.de>
2025-01-20 14:54:03 +0100

    feat: hello-world.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HelloWorldComponent} from './hello-world.component';
 
 @Component({
   selector: 'pl-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HelloWorldComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
